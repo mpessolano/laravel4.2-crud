8c2d59e63ac5322715f02417468f913e8832a2a1
Marianna Pessolano <marianna.pessolano@gmail.com>
2019-03-28 19:53:31 -0400

    [#2] Creación y configuración de la base de datos
    CREATE DATABASE `laravel4.2-crud` COLLATE 'utf8_unicode_ci';

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'forge',
-			'username'  => 'forge',
-			'password'  => '',
+			'database'  => 'laravel4.2-crud',
+			'username'  => 'root',
+			'password'  => 'pass',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
