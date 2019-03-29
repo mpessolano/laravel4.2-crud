be291f0dd083c29ad965338870dcff426345a9cd
Marianna Pessolano <marianna.pessolano@gmail.com>
2019-03-28 23:49:51 -0400

    [#5] Creación de vistas create.blade.php, edit.blade.php, index.blade.php y layout master.blade.php
    Modificación del método create en el controlador para mostrar la vista create.blade.php

---
+++ app/views/books/create.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.master')
+
+@section('content')
+<div class="card">
+	<h5 class="card-header">
+		Add Book
+	</h5>
+	<div class="card-body">
+		@if ($errors->any())
+			<div class="alert alert-danger">
+				<ul>
+					@foreach ($errors->all() as $error)
+						<li>{{ $error }}</li>
+					@endforeach
+				</ul>
+			</div>
+		@endif
+		<form method="post" action="{{ route('books.store') }}">
+			<div class="form-group">
+				{{ Form::token() }}
+				<label for="name">Name:</label>
+				<input type="text" class="form-control" name="name" id="name">
+			</div>
+			<div class="form-group">
+				<label for="isbn">ISBN:</label>
+				<input type="text" class="form-control" name="isbn" id="isbn">
+			</div>
+			<div class="form-group">
+				<label for="price">Price:</label>
+				<input type="text" class="form-control" name="price" id="price">
+			</div>
+			<button type="submit" class="btn btn-primary">Create Book</button>
+		</form>
+	</div>
+</div>
+@stop
---
+++ app/views/layouts/master.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>Laravel 4.2 CRUD</title>
+	<link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+</head>
+<body class="py-5">
+	<div class="container">
+		@yield('content')
+	</div>
+	<script src="{{ asset('js/jquery-3.3.1.slim.min.js') }}"></script>
+	<script src="{{ asset('js/popper.min.js') }}"></script>
+	<script src="{{ asset('js/bootstrap.min.js') }}"></script>
+</body>
+</html>
